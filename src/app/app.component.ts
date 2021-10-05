fee345fd94cfd576605feca566b4d3bf754ec2d3
Alejandro Granados <alegranados03@gmail.com>
2021-10-05 14:39:18 -0600

    menu lateral dinamico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 
+interface IMenuItem {
+  icon: string;
+  name: string;
+  url: string;
+}
+// más iconos: https://www.angularjswiki.com/angular/angular-material-icons-list-mat-icon-list/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'proyectosic';
   showFiller = false;
+  menu: IMenuItem[] = [
+    {
+      icon: 'assignment',
+      name: 'Menu Item 1',
+      url: '',
+    },
+    {
+      icon: 'card_travel',
+      name: 'Menu Item 2',
+      url: '',
+    },
+    {
+      icon: 'date_range',
+      name: 'Menu Item 3',
+      url: '',
+    },
+    {
+      icon: 'attach_money',
+      name: 'Menu Item 4',
+      url: '',
+    },
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import { InicioComponent } from './pages/inicio/inicio.component';
 @NgModule({
   declarations: [
     AppComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
